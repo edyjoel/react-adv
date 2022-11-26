1b229fdb5c21ddc8c3f55d027b8de2f136be28a8
Edy <edyxicon@gmail.com>
2022-11-26 15:08:10 -0600

    Component Extend Styles - listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,6 +5,8 @@ import {
   ProductTitle,
 } from "../components/";
 
+import '../styles/custom-styles.css';
+
 const product = {
   id: "1",
   title: "Coffee Mug",
@@ -17,12 +19,28 @@ export const ShoppingPage = () => {
       <h1>ShoppingPage</h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <ProductCard product={product}>
-          <ProductCard.Image />
-          <ProductCard.Title title={""} />
-          <ProductCard.Buttons />
+        <ProductCard
+          product={product}
+          className="bg-dark text-white"
+        >
+          <ProductCard.Image className="custom-image" />
+          <ProductCard.Title title={""} activeClass="active" />
+          <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
-        <ProductCard product={product}>
+        <ProductCard
+          product={product}
+          className="bg-dark text-white"
+        >
+          <ProductImage className="custom-image" />
+          <ProductTitle title={""} className="text-white" activeClass="active" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+        <ProductCard
+          product={product}
+          style={{
+            backgroundColor: "#70D1F8",
+          }}
+        >
           <ProductImage />
           <ProductTitle title={""} />
           <ProductButtons />
